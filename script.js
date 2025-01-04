1065cdaab81fb10a70f534a2d1ce895e6eb5ac82
Ezrqn Kemboi <ezrqnkemboi@gmail.com>
2025-01-04 13:46:16 +0300

    chore: add form submission

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ function scrollGallery(direction) {
 setInterval(() => {
   scrollGallery(1);
 }, 3000);
+
+const contactForm = document.getElementById('contactForm');
+contactMe(contactForm);
+
+function contactMe(form) {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault(); // prevennt from default submitting
+    const name = form.name.value.trim();
+    const email = form.email.value.trim();
+    const message = form.message.value.trim();
+
+    if(!name || !email || !message) {
+      alert('All fields are required');
+      return;
+    }
+
+    console.log("Form submitted", {
+      name, 
+      email, 
+      message
+    });
+
+    alert(`Thank you for your message, ${name}`);
+    // reset form
+    form.reset();
+    // to submit to actual form, can make use of form.submit()
+    return ;
+  })
+ 
+
+}
